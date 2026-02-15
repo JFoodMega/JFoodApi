eb1f434ca4dda247047c834374174b9a76668d83
Nikolay Shulgin <nikolaysh2005@gmail.com>
2026-02-15 20:38:53 +0400

    changed a bit more

--- src/main/java/com/jfood/jFood/dish/repository/DishRepository.java
+++ src/main/java/com/jfood/jFood/dish/repository/DishRepository.java
@@ -1,8 +1,8 @@
-package com.jfood.jFood.dishes.repository;
+package com.jfood.jFood.dish.repository;
 
-import com.jfood.jFood.dishes.model.Client;
+import com.jfood.jFood.dish.model.Dish;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ClientRepository extends JpaRepository<Client,Long> {
+public interface DishRepository extends JpaRepository<Dish,Long> {
 
 }
