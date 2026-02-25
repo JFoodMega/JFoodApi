fc002a7ec99268b5c168d7854ae216850e730afe
Nikolay Shulgin <nikolaysh2005@gmail.com>
2026-02-25 19:19:48 +0400

    коммит для максона

--- src/main/java/com/jfood/jFood/moderator/model/Moderator.java
+++ src/main/java/com/jfood/jFood/moderator/model/Moderator.java
@@ -1,4 +1,31 @@
 package com.jfood.jFood.moderator.model;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "moderators")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class Moderator {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true, nullable = false)
+    private String login;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String phone;
 }
